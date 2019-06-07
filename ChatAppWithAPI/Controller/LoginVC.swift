c847de5892964d6a9513deeaf1d6a30c83457b71
Huy Vu <vaart12345@gmail.com>
2019-06-07 11:53:04 +0700

    adding change to a new branch named newBranch.

---
+++ ChatAppWithAPI/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  ChatAppWithAPI
+//
+//  Created by Huy Vu on 6/7/19.
+//  Copyright © 2019 Huy Vu. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
