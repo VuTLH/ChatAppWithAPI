937e89e89a8cb584c21d0ed83ed59d4134d4ae59
Huy Vu <vaart12345@gmail.com>
2019-06-07 11:18:49 +0700

    commit when implementing in le4.

--- ChatAppWithAPI/Controller/ChannelVC.swift
+++ ChatAppWithAPI/Controller/ChannelVC.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ChannelVC: UIViewController {
-
+    
+    //outlets
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60 //set cho rear view khi hiện ra thì front view width chỉ còn 60
         
     }
-
+    
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
 
 }
---
+++ ChatAppWithAPI/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ChatAppWithAPI
+//
+//  Created by Huy Vu on 6/7/19.
+//  Copyright © 2019 Huy Vu. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
