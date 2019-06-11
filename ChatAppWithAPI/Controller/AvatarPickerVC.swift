9ae77fe66121ae843ec79972941c54b7e09cba14
Huy Vu <vaart12345@gmail.com>
2019-06-11 16:25:44 +0700

    end le 14.

--- ChatAppWithAPI/Controller/AvatarPickerVC.swift
+++ ChatAppWithAPI/Controller/AvatarPickerVC.swift
@@ -14,6 +14,8 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    //variables
+    var avatarType = AvatarType.dark
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +28,37 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBAction func backPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    @IBAction func segmentValueChanged(_ sender: Any) {
-        
+    @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            avatarType = .dark
+        }else {
+            avatarType = .light
+        }
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var numberOfColumns: CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            //320 la c.rong cua iphone S.E nho nhat trong cac loai
+            numberOfColumns = 4
+        }
+        let spaceBetweenCell: CGFloat = 10
+        let padding: CGFloat = 40 //khoảng cách giữa cell với mép trái phải
+        let cellDimension = ((collectionView.bounds.width - padding) - (numberOfColumns - 1) * spaceBetweenCell) / numberOfColumns
+        /*
+         tính kích thước của cell = chiều rộng của 3 or 4 column chia cho số column
+         */
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        }else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -40,6 +71,7 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AVATAR_CELL, for: indexPath) as? AvatarCell {
+            cell.configureCell(index: indexPath.item, type: avatarType)
             return cell
         }
         return AvatarCell()
